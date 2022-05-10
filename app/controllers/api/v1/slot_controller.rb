155a8ad2fa38916e0b51633f0ee1c49ef96751fc
Efe Agare <knowledgeagare157@gmail.com>
2022-05-10 22:05:26 +0100

    create a slot controller

---
+++ app/controllers/api/v1/slot_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::SlotController < ApplicationController
+  def index
+  end
+
+  def create
+    
+  end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'api/v1/slot#index'
+
+  namespace :api do
+    namespace :v1 do
+     resources :slot
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
