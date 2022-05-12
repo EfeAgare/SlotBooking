a8bd4a57b19d493cc509a62e89aa387963f4b898
Efe Agare <knowledgeagare157@gmail.com>
2022-05-13 00:14:20 +0100

    add spec test

--- spec/requests/api/v1/slot_spec.rb
+++ spec/requests/api/v1/slot_spec.rb
@@ -1,7 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Slots", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+  describe "#index" do
+    let(:date) { Time.zone.tomorrow }
+    let(:duration) { 40 }
+
+    subject { get "/api/v1/slot", params: { date: date, 
+      duration: duration, format: "application/json" }
+    }
+
+    context "when no available plan exist" do
+      it "returns http success" do
+        subject
+
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns the available slots" do
+        subject
+
+        expect(JSON.parse(response.body)).not_to be_nil
+        expect(JSON.parse(response.body)).to include("8:00AM")
+      end
+    end
+  end
+
+  describe "#create" do
+    let(:date) { Time.zone.tomorrow }
+    let(:duration) { 40 }
+    let(:slot) {"9:00AM" }
+    
+    subject { post "/api/v1/slot", params: { date: date, 
+      duration: duration, slot: slot, format: "application/json" } }
+
+    before do
+      AvailableSlot.create(start: "8:00AM", end: "4:45PM", generated_at: date)
+    end
+
+    it "returns http success" do
+      
+      subject
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns the slots base on the duration" do
+      subject
+     
+      expect(JSON.parse(response.body)["time_range"]).to include("9:00AM")
+      expect(JSON.parse(response.body)["time_range"]).to include("9:15AM")
+      expect(JSON.parse(response.body)["time_range"]).to include("9:30AM")
+    end
   end
 end
